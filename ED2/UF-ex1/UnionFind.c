03632d8c475a245481456a47c9419311309a8f03
umishell <umi.shell6@gmail.com>
2024-03-11 15:23:28 -0300

    MISC

--- ED2/UF-ex1/UnionFind.c
+++ ED2/UF-ex1/UnionFind.c
@@ -3,109 +3,112 @@
 
 typedef struct
 {
-  int *id;
-  int count;
-  int n;
+	int* id;
+	int count;
+	int n;
 } UF;
 
-UF *init_UF(int n);
-int count_UF(UF *uf);
-int connected_UF(UF *uf, int p, int q);
-int find_UF(UF *uf, int p);
-void union_UF(UF *uf, int p, int q);
+UF* init_UF(int n);
+int count_UF(UF* uf);
+int connected_UF(UF* uf, int p, int q);
+int find_UF(UF* uf, int p);
+void union_UF(UF* uf, int p, int q);
 
-UF *init_UF(int n)
+UF* init_UF(int n)
 {
-  int i;
-  UF *uf;
+	int i;
+	UF* uf;
 
-  uf = malloc(sizeof(UF));
+	uf = malloc(sizeof(UF));
 
-  uf->id = malloc(sizeof(int) * n);
-  uf->n = n;
-  uf->count = n;
-  for (i = 0; i < uf->count; ++i)
-  {
-    uf->id[i] = i;
-  }
+	uf->id = malloc(sizeof(int) * n);
+	uf->n = n;
+	uf->count = n;
+	for (i = 0; i < uf->count; ++i)
+	{
+		uf->id[i] = i;
+	}
 
-  return (uf);
+	return (uf);
 }
 
-int count_UF(UF *uf)
+int count_UF(UF* uf)
 {
-  return (uf->count);
+	return (uf->count);
 }
 
-int connected_UF(UF *uf, int p, int q)
+int connected_UF(UF* uf, int p, int q)
 {
-  return (find_UF(uf, p) == find_UF(uf, q));
+	return (find_UF(uf, p) == find_UF(uf, q));
 }
 
-int find_UF(UF *uf, int p)
+int find_UF(UF* uf, int p)
 {
-  return (uf->id[p]);
+	return (uf->id[p]);
 }
 
-void union_UF(UF *uf, int p, int q)
+void union_UF(UF* uf, int p, int q)
 {
-  uf->count--;
-  int h, base1 = uf->id[p], base2 = uf->id[q];
+	uf->count--;
+	int h, base1 = uf->id[p], base2 = uf->id[q];
 
-  if (base1 < base2)
-  {
-    if (base1 >= 0)
-      uf->id[p] = -base1;
-    uf->id[q] = uf->id[p];
-  }
-  else
-  {
-    if (base2 >= 0)
-      uf->id[q] = -base2;
-    uf->id[p] = uf->id[q];
-  }
+	if (base1 < base2)
+	{
+		if (base1 >= 0)
+			uf->id[p] = -base1;
+		uf->id[q] = uf->id[p];
+	}
+	else
+	{
+		if (base2 >= 0)
+			uf->id[q] = -base2;
+		uf->id[p] = uf->id[q];
+	}
 
-  if (base1 < 0 && base2 < 0)
-  {
-    for (h = 0; h < uf->n; ++h)
-    {
-      if (uf->id[h] == base2)
-        uf->id[h] = base1;
-    }
-  }
+	if (base1 < 0 && base2 < 0)
+	{
+		for (h = 0; h < uf->n; ++h)
+		{
+			if (uf->id[h] == base2)
+				uf->id[h] = base1;
+		}
+	}
 
-  return;
+	return;
 }
 
 int main()
 {
-  int n, p, q, i, k;
-  UF *uf;
-  
-  scanf("%d", &n);
-  int result[n][2];
-  
-  uf = init_UF(n);
+	int n, p, q, i, k;
+	UF* uf;
 
-  scanf("%d %d", &p, &q);
-  while (p > -1 && q > -1)
-  {
+	scanf("%d", &n);
+	int result[111][2];
 
-    if (!connected_UF(uf, p, q))
-    {
-    result[i][0]=p;
-    result[i][1]=q;
-    i++;
-      union_UF(uf, p, q);
-    }
-    scanf("%d %d", &p, &q);
-  }
+	uf = init_UF(n);
 
-  for (int j=0;j<n;j++){
-    for (int k=0;k<2;k++){ 
-        if (!(result[j][0]==0 && result[j][1]==0)){
-            printf("%d ",result[j][k]);}
-  }printf("\n");}
-  
-  printf("%d components\n", uf->count);
+	scanf("%d %d", &p, &q);
+	while (p > -1 && q > -1)
+	{
+
+		if (!connected_UF(uf, p, q))
+		{
+			result[i][0] = p;
+			result[i][1] = q;
+			i++;
+			union_UF(uf, p, q);
+		}
+		scanf("%d %d", &p, &q);
+
+	}
+
+
+	for (int j = 0; j < n; j++) {
+		if (result[j][0] != 0 || result[j][1] != 0) {
+			printf("%d %d\n", result[j][0], result[j][1]);
+		}
+	}
+
+
+	printf("%d components\n", uf->count);
 }
