14bfb3aa6f135b56613c340d8634aef5cf5fcc6e
umishell <umi.shell6@gmail.com>
2024-03-10 23:45:28 -0300

    misc

--- ED2/UF-ex1/UF-ex1.cpp
+++ ED2/UF-ex1/UF-ex1.cpp
@@ -1,20 +1,95 @@
-// UF-ex1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
+typedef struct
+{
+    int* id;
+    int count;
+    int n;
+} UF;
+
+UF* init_UF(int n);
+int count_UF(UF* uf);
+int connected_UF(UF* uf, int p, int q);
+int find_UF(UF* uf, int p);
+void union_UF(UF* uf, int p, int q);
+
+UF* init_UF(int n)
+{
+    int i;
+    UF* uf;
+
+    uf = malloc(sizeof(UF));
+
+    uf->id = malloc(sizeof(int) * n);
+    uf->n = n;
+    uf->count = n;
+    for (i = 0; i < uf->count; ++i)
+    {
+        uf->id[i] = i;
+    }
+
+    return (uf);
+}
+
+int count_UF(UF* uf)
+{
+    return (uf->count);
+}
+
+int connected_UF(UF* uf, int p, int q)
+{
+    return (find_UF(uf, p) == find_UF(uf, q));
+}
+
+int root(UF* uf, int p)
+{
+    while (uf->id[p] != p)
+        p = uf->id[p];
+    return p;
+}
+
+int find_UF(UF* uf, int p)
+{
+    return (root(uf, p));
+}
+
+void union_UF(UF* uf, int p, int q)
+{
+    uf->count--;
+    int root_A = root(uf, p);
+    int root_B = root(uf, q);
+    uf->id[root_A] = root_B;
+    return;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n, p, q;
+    UF* uf;
+
+    scanf("%d", &n);
+
+    uf = init_UF(n);
+
+    scanf("%d %d", &p, &q);
+    while (p > -1 && q > -1)
+    {
+
+        if (!connected_UF(uf, p, q))
+        {
+            printf("%d %d\n", p, q);
+            union_UF(uf, p, q);
+        }
+
+        // printf("\n\n");
+        // for (int j = 0; j < uf->n; ++j)
+        // {
+        //   printf("%d -> %d\n", j, uf->id[j]);
+        // }
+
+        scanf("%d %d", &p, &q);
+    }
+
+    printf("%d components\n", uf->count);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
