711fd7945f0086b52d55b2e70db8f9d52bc6b2f1
umishell <umi.shell6@gmail.com>
2024-03-25 18:11:18 -0300

    tarefa robo supermercado

--- ED2/tarefas/tarefas.cpp
+++ ED2/tarefas/tarefas.cpp
@@ -3,28 +3,85 @@
 
 using namespace std;
 
+int ar[10]{};
+
+
+struct IndexMax {
+    
+    int index;
+    int max;
+ 
+};
+
+
+    int goRight(int index) {
+        int max = 0;
+        int length = sizeof(ar) / sizeof(ar[0]);
+            for (int i = index; i < length; i++) {
+                if (ar[i] == 0) return max;
+                else {
+                    max++;
+                }
+            }
+        return max;
+    }
+
+    int goLeft(int index) {
+        int max = 0;
+            for (int i = index; i > -1; i--) {
+                if (ar[i] == 0) return max;
+                else {
+                    max++;
+                }
+            }
+        return max;
+    }
+    IndexMax  leftright() {
+        IndexMax indexmax;
+        for (int i = 0; i < 10; i++) {
+
+            int left = goLeft(i);
+            int right = goRight(i);
+            if (right > left) indexmax.max = right;
+            else indexmax.max = left;
+
+        }
+        return indexmax;
+    }
+    IndexMax  leftright() {
+        IndexMax indexmax;
+        for (int i = 0; i < 10; i++) {
+
+            int left = goLeft(i);
+            int right = goRight(i);
+            if (right > left ) indexmax.max = right;
+            else indexmax.max = left;
+
+        }
+        return indexmax;
+    }
 
 
 int main() {
 
-    int array[10]{};
+   
     int totalItens = 20;
 
     srand(time(0));
 
     while (totalItens > 0) {
         int randomNumber = rand() % 10;
-        if (array[randomNumber] > 2) {
+        if (ar[randomNumber] > 2) {
             continue;
         }
         else {
-            array[randomNumber]++;
+            ar[randomNumber]++;
             totalItens--;
         }
     }
 
     for (int i = 0; i < 10; i++) {
-        cout << array[i] << endl;
+        cout << ar[i];
     }
     return 0;
 }
