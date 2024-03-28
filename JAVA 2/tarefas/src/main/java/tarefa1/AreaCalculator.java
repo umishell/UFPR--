5ac65393b62f4034eedbd471c7235a9935a76097
umishell <umi.shell6@gmail.com>
2024-03-28 13:25:51 -0300

    changing  intellij loo2 tarefas project to maven build

--- JAVA 2/tarefas/src/main/java/tarefa1/AreaCalculator.java
+++ JAVA 2/tarefas/src/main/java/tarefa1/AreaCalculator.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 public class AreaCalculator {
 
-    public static void isNonNegative(double number) throws RuntimeException {
+    public static void isNegative(double number) throws RuntimeException {
         if (number < 0) {
-            throw new RuntimeException("Number must be non-negative");
+            throw new RuntimeException("Must be a positive number");+
+
         }
     }
 
@@ -19,7 +20,7 @@ public class AreaCalculator {
         return side1 * side2 / 2;
     }
 
-    public static double circunferenceArea(double radius) {
+    public static double circumferenceArea(double radius) {
         return Math.PI * radius * radius;
     }
 
@@ -45,7 +46,7 @@ public class AreaCalculator {
             choice = scan.nextInt();
         }
 
-        if (choice != 1 && choice != 2 && choice != 3) {  // Use && for logical AND
+        if (choice < 1 &&  choice > 3) {  // Use && for logical AND
             System.out.println("type the number options");
         }
 
@@ -56,28 +57,28 @@ public class AreaCalculator {
                     System.out.println("type two side numbers :");
                     System.out.println("side one:");
                     double side1 = scan.nextDouble();
-                    isNonNegative(side1);
+                    isNegative(side1);
                     System.out.println("side two:");
                     double side2 = scan.nextDouble();
-                    isNonNegative(side2);
+                    isNegative(side2);
                     System.out.println("area of triangle is " + triangleArea(side1, side2) + " square meters");
                 }
                 case 2 -> {
                     System.out.println("type two side numbers :");
                     System.out.println("side one:");
                     double side1 = scan.nextDouble();
-                    isNonNegative(side1);
+                    isNegative(side1);
                     System.out.println("side two:");
                     double side2 = scan.nextDouble();
-                    isNonNegative(side2);
+                    isNegative(side2);
                     System.out.println("area of triangle is " + rectangleArea(side1, side2) + " square meters");
                 }
                 case 3 -> {
                     System.out.println("type radius :");
                     double radius = scan.nextDouble();
-                    isNonNegative(radius);
-                    isNonNegative(radius);
-                    System.out.println("area of circle is " + String.format("%.2f", circunferenceArea(radius)) + " square meters");
+                    isNegative(radius);
+                    isNegative(radius);
+                    System.out.println("area of circle is " + String.format("%.2f", circumferenceArea(radius)) + " square meters");
                 }
             }
         scan.close();
